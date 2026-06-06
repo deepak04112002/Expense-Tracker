37abf4ca24c6c920a698030cdae3c1ec141264d3
Deepak  Kumar Majhi <dmajhi286@gmail.com>
2026-06-06 09:52:06 +0530

    fix(db): fix tsconfig and exports for Vercel build

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
-import {config} from "dotenv"
+import { config } from "dotenv";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 config({ path: resolve(__dirname, "../.env") });
 
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prismaClient = new PrismaClient({ adapter });
-export * from "../generated/prisma/client";
+export * from "../generated/prisma/client.js";
